97497f1bf85d60f49db178cb3dde93faa7f8ab75
Philip Bengtsson <philip@ditgear.com>
2025-01-25 22:37:02 +0100

    fix(types): fix errors in merging datalogs

--- src/shared/utils/datalog-merge.ts
+++ src/shared/utils/datalog-merge.ts
@@ -26,62 +26,62 @@ export const mergeDatalogs = (datalogs: DatalogType[]): DatalogType => {
   merged.date = minDate === maxDate ? minDate : `${minDate} - ${maxDate}`
 
   // --- OCF ---
-  if (propertyIsSetOnAny(datalogs, (d) => d.ocf.files)) {
-    merged.ocf.files = getTotalFiles(wrapped, 'ocf')
+  if (propertyIsSetOnAny(datalogs, (d) => d.ocf?.files)) {
+    merged.ocf && (merged.ocf.files = getTotalFiles(wrapped, 'ocf'))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.ocf.size)) {
-    merged.ocf.size = getTotalSize(wrapped, 'ocf')
+  if (propertyIsSetOnAny(datalogs, (d) => d.ocf?.size)) {
+    merged.ocf && (merged.ocf.size = getTotalSize(wrapped, 'ocf'))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.ocf.duration)) {
-    merged.ocf.duration = getTotalDuration(wrapped, 'tc')
+  if (propertyIsSetOnAny(datalogs, (d) => d.ocf?.duration)) {
+    merged.ocf && (merged.ocf.duration = getTotalDuration(wrapped, 'tc'))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.ocf.reels)) {
+  if (propertyIsSetOnAny(datalogs, (d) => d.ocf?.reels)) {
     const reelsSet = new Set<string>()
     wrapped.forEach((w) => w.ocf.reels().forEach((r) => reelsSet.add(r)))
-    merged.ocf.reels = Array.from(reelsSet)
+    merged.ocf && (merged.ocf.reels = Array.from(reelsSet))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.ocf.copies)) {
+  if (propertyIsSetOnAny(datalogs, (d) => d.ocf?.copies)) {
     const copiesSet = new Set<string>()
     wrapped.forEach((w) => {
       w.ocf.copies().forEach((c) => {
         c.volumes.forEach((d) => copiesSet.add(d))
       })
     })
-    merged.ocf.copies = Array.from(copiesSet)
+    merged.ocf && (merged.ocf.copies = Array.from(copiesSet))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.ocf.clips)) {
-    merged.ocf.clips = datalogs.flatMap((log) => log.ocf.clips ?? [])
+  if (propertyIsSetOnAny(datalogs, (d) => d.ocf?.clips)) {
+    merged.ocf && (merged.ocf.clips = datalogs.flatMap((log) => log.ocf?.clips ?? []))
   }
 
   // --- PROXY ---
-  if (propertyIsSetOnAny(datalogs, (d) => d.proxy.files)) {
-    merged.proxy.files = getTotalFiles(wrapped, 'proxy')
+  if (propertyIsSetOnAny(datalogs, (d) => d.proxy?.files)) {
+    merged.proxy && (merged.proxy.files = getTotalFiles(wrapped, 'proxy'))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.proxy.size)) {
-    merged.proxy.size = getTotalSize(wrapped, 'proxy')
+  if (propertyIsSetOnAny(datalogs, (d) => d.proxy?.size)) {
+    merged.proxy && (merged.proxy.size = getTotalSize(wrapped, 'proxy'))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.proxy.clips)) {
-    merged.proxy.clips = datalogs.flatMap((log) => log.proxy.clips ?? [])
+  if (propertyIsSetOnAny(datalogs, (d) => d.proxy?.clips)) {
+    merged.proxy && (merged.proxy.clips = datalogs.flatMap((log) => log.proxy?.clips ?? []))
   }
 
   // --- SOUND ---
-  if (propertyIsSetOnAny(datalogs, (d) => d.sound.files)) {
-    merged.sound.files = getTotalFiles(wrapped, 'sound')
+  if (propertyIsSetOnAny(datalogs, (d) => d.sound?.files)) {
+    merged.sound && (merged.sound.files = getTotalFiles(wrapped, 'sound'))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.sound.size)) {
-    merged.sound.size = getTotalSize(wrapped, 'sound')
+  if (propertyIsSetOnAny(datalogs, (d) => d.sound?.size)) {
+    merged.sound && (merged.sound.size = getTotalSize(wrapped, 'sound'))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.sound.copies)) {
+  if (propertyIsSetOnAny(datalogs, (d) => d.sound?.copies)) {
     const copiesSet = new Set<string>()
     wrapped.forEach((w) => {
       w.ocf.copies().forEach((c) => {
         c.volumes.forEach((d) => copiesSet.add(d))
       })
     })
-    merged.sound.copies = Array.from(copiesSet)
+    merged.sound && (merged.sound.copies = Array.from(copiesSet))
   }
-  if (propertyIsSetOnAny(datalogs, (d) => d.sound.clips)) {
-    merged.sound.clips = datalogs.flatMap((log) => log.sound.clips ?? [])
+  if (propertyIsSetOnAny(datalogs, (d) => d.sound?.clips)) {
+    merged.sound && (merged.sound.clips = datalogs.flatMap((log) => log.sound?.clips ?? []))
   }
 
   return merged
